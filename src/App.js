9a828151f00a1f8328c1d819f4f58176df6c55fe
AA_Arnold <amasoharnold@gmail.com>
2020-08-29 04:40:09 -1100

    successfully fetched from api and displayed in app with bunch of side effects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = React.useState([])
+
+  React.useEffect(() => {
+
+    const getCountriesAndIso = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(({ country, countryInfo: { iso2, _id } }) => ({ name: country, value: iso2, id: _id }))
+          // console.log(countries)
+          setCountries(countries)
+        })
+
+
+    }
+
+    // const getCountriesAndIso = fetch('https://disease.sh/v3/covid-19/countries')
+    //   .then(response => response.json())
+    //   .then(data => data.map(({ country, countryInfo: { iso2 } }) => {
+    //     return country, iso2
+    //   }))
+
+
+
+    // setCountries(country)
+    getCountriesAndIso()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='app__header'>
+        <h1>COVDI-19 TRACKER</h1>
+        <FormControl className='app__dropdown'>
+
+          <MenuItem value='worldwide'>WORLD WIDE</MenuItem>
+          <Select value='' variant='outline'>
+            {
+              countries.map(countries => <MenuItem key={countries.id} value={countries.values}>{countries.name}</MenuItem>)
+            }
+          </Select>
+
+        </FormControl>
+      </div>
+
     </div>
   );
 }
