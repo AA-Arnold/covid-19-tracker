bad2298297f48ba0bb298d8de83c37e593c5ae50
AA_Arnold <amasoharnold@gmail.com>
2020-12-15 22:26:30 -1100

    Updates

---
+++ precache-manifest.6665fabc050432101a76fd7567aadb0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1beae0bf46affcd7cb071a78fdbcb6",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "011626675c7e73052f18",
+    "url": "/covid-19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "b6e23e477ac41a7617ea",
+    "url": "/covid-19-tracker/static/css/main.bf30770e.chunk.css"
+  },
+  {
+    "revision": "011626675c7e73052f18",
+    "url": "/covid-19-tracker/static/js/2.8a81a048.chunk.js"
+  },
+  {
+    "revision": "8b14d2fcaa351a227943ea567ad28808",
+    "url": "/covid-19-tracker/static/js/2.8a81a048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e23e477ac41a7617ea",
+    "url": "/covid-19-tracker/static/js/main.60a2118d.chunk.js"
+  },
+  {
+    "revision": "672292a1fe2b7776fa34",
+    "url": "/covid-19-tracker/static/js/runtime-main.47f4a27f.js"
+  }
+]);
